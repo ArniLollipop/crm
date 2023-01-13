6d9d79d7d7e5248577c19e787aa7e461394aa439
ArniLollipop <bocunahero@gmail.com>
2023-01-13 19:40:45 +0600

    first push

---
+++ src/pages/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <div className='w-full min-h-[50vh] flex items-center justify-center'>
+      <div className=' p-5 rounded-xl bg-gray-400 max-w-[768px] mx-auto'>
+        <p className='text-2xl text-white'>Авторизация</p>
+        
+      </div>
+    </div>
+  );
+}
+
+export default Login;
